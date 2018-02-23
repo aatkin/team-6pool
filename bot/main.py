a27386d6aa08286271ebb7672ee9c6cb90bc8d8d
Santeri Hiltunen <accounts.github@me.hilzu.moe>
2018-02-23 11:24:24 +0200

    Simplify double overlord logic

--- bot/main.py
+++ bot/main.py
@@ -12,17 +12,12 @@ def seconds_to_ticks(s):
     return s * 21.5
 
 
-overlord_build_time = seconds_to_ticks(18)
-
-
 class MyBot(sc2.BotAI):
     with open(Path(__file__).parent / "../botinfo.json") as f:
         NAME = json.load(f)["name"]
 
     def __init__(self):
         self.drone_counter = 0
-        self.overlord_started = 0
-        self.building_overlord = False
 
     async def on_step(self, iteration):
         hatchery = self.units(HATCHERY).ready.first
@@ -31,19 +26,14 @@ class MyBot(sc2.BotAI):
         if iteration % 100 == 0:
             pprint(vars(self))
 
-        if (iteration - self.overlord_started) >= overlord_build_time:
-            self.building_overlord = False
-
         if iteration == 0:
             await self.chat_send(f"Name: {self.NAME}")
 
         if iteration == 666:
             await self.chat_send("666 HELLFIRE")
 
-        if self.supply_left < 3 and not self.building_overlord:
+        if self.supply_left < 3 and not self.already_pending(OVERLORD):
             if self.can_afford(OVERLORD) and larvae.exists:
-                self.overlord_started = iteration
-                self.building_overlord = True
                 await self.do(larvae.random.train(OVERLORD))
 
         if self.drone_counter < 5:
