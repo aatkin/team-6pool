b6517fde6be4a0bc45e20657da32d3e804510053
Anssi Kinnunen <anssi.kinnunen@reaktor.com>
2018-02-23 13:01:24 +0200

    Add some expansion logic

--- bot/expansion.py
+++ bot/expansion.py
@@ -1,4 +1,101 @@
+import json
+from pathlib import Path
+from pprint import pprint
 
-if self.can_afford(HATCHERY) and larvae.exists and self.hatchery_counter < 5:
-    location = await self.get_next_expansion()
-    await self.build(HATCHERY, location)
+import sc2
+from sc2 import Race, Difficulty
+from sc2.constants import *
+from sc2.player import Bot, Computer
+
+
+def seconds_to_ticks(s):
+    return s * 21.5
+
+def count_units(self, unit):
+    all_units = self.units(unit).ready.amount + self.units(unit).not_ready.amount
+    return all_units
+
+class MyBot(sc2.BotAI):
+    with open(Path(__file__).parent / "../botinfo.json") as f:
+        NAME = json.load(f)["name"]
+
+    async def create_queens(self, iteration, hatchery):
+        if self.can_afford(QUEEN) and self.queen_counter < 2:
+            queue_full = await self.do(hatchery.train(QUEEN))
+            if not queue_full:
+                self.queen_counter += 1
+
+    async def handle_queens(self, iteration, hatchery):
+        # queen larva injection
+        for queen in self.units(QUEEN).idle:
+            abilities = await self.get_available_abilities(queen)
+            if AbilityId.EFFECT_INJECTLARVA in abilities:
+                await self.do(queen(EFFECT_INJECTLARVA, hatchery))
+
+    # build one extractor
+    async def create_extractor(self, iteration, hatchery, minerals_nicely_saturated):
+        if not self.already_pending(EXTRACTOR) and len(self.units(EXTRACTOR)) < 1 and minerals_nicely_saturated:
+            if self.can_afford(EXTRACTOR):
+                drone = self.workers.random
+                target = self.state.vespene_geyser.closest_to(drone.position)
+                err = await self.do(drone.build(EXTRACTOR, target))
+                if not err:
+                    self.extractor_started = True
+
+    # move workers to gas if havent done that yet
+    async def move_workers_to_extractor(self, iteration, extractor):
+        if self.units(EXTRACTOR).ready.exists:
+            required_harvesters = max(0, extractor.ideal_harvesters - extractor.assigned_harvesters)
+            for drone in self.workers.random_group_of(required_harvesters):
+                await self.do(drone.gather(extractor))
+
+    def __init__(self):
+        self.drone_counter = 0
+        self.queen_counter = 0
+        self.expansion_counter = 0
+
+    async def on_step(self, iteration):
+        # hatchery = self.units(HATCHERY).ready.first
+        larvae = self.units(LARVA)
+
+        if iteration % 100 == 0:
+            pprint(vars(self))
+
+        if iteration == 0:
+            await self.chat_send(f"Name: {self.NAME}")
+
+        if iteration == 666:
+            await self.chat_send("666 HELLFIRE")
+
+        # build more overlords
+        if self.supply_left < 3 and not self.already_pending(OVERLORD):
+            if self.can_afford(OVERLORD) and larvae.exists:
+                await self.do(larvae.random.train(OVERLORD))
+
+        for extractor in self.units(EXTRACTOR).ready:
+            self.move_workers_to_extractor(iteration, extractor)
+
+        for hatchery in self.units(HATCHERY):
+            minerals_nicely_saturated = hatchery.ideal_harvesters - hatchery.assigned_harvesters is not 0
+
+            # build drones
+            if not minerals_nicely_saturated:
+                if self.can_afford(DRONE) and self.supply_left >= 1 and larvae.exists:
+                    self.drone_counter += 1
+                    await self.do(larvae.random.train(DRONE))
+
+            # build spawning pool
+            if not self.units(SPAWNINGPOOL) and not self.already_pending(SPAWNINGPOOL):
+                if self.can_afford(SPAWNINGPOOL):
+                    worker = self.select_build_worker(hatchery, force=True)
+                    await self.build(SPAWNINGPOOL, hatchery, unit=worker)
+
+            await self.create_queens(iteration, hatchery)
+            await self.handle_queens(iteration, hatchery)
+            await self.create_extractor(iteration, hatchery, minerals_nicely_saturated)
+
+        # expansion when affordable
+        if self.units(SPAWNINGPOOL) and self.can_afford(HATCHERY) and self.expansion_counter < 1:
+            location = await self.get_next_expansion()
+            await self.build(HATCHERY, location)
+            self.expansion_counter += 1
